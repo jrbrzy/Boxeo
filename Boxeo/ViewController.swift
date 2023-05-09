45c4dd25558f99542ac02d2e95abd789eec1ef76
Jairo Munoz <jrbrzy@icloud.com>
2023-05-09 10:15:41 -0600

    Added Background yay

--- Boxeo/ViewController.swift
+++ Boxeo/ViewController.swift
@@ -12,8 +12,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignbackground()
     }
 
+    func assignbackground()
+    {
+            let background = UIImage(named: "boxingRing")
+
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+        
+//        let backgroundImage = UIImage(named: "boxingRing")
+//        var backgroundImageView : UIImageView!
+//        backgroundImageView.image = backgroundImage
+//        backgroundImageView.contentMode = .scaleAspectFill
+        imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+       
+        
+        
+        
+    }
+    
+
+    
+    
+    
+    
+    
 
 }
 
