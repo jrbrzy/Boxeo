0322ebcff3508a5461839cb6ad09d30ef4d10854
Jairo Munoz <jrbrzy@icloud.com>
2023-05-11 10:24:03 -0600

    Spaced them

--- Boxeo/ViewController.swift
+++ Boxeo/ViewController.swift
@@ -8,43 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         assignbackground()
     }
-
+    
     func assignbackground()
     {
-            let background = UIImage(named: "boxingRing")
-
-            var imageView : UIImageView!
-            imageView = UIImageView(frame: view.bounds)
-            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-            imageView.clipsToBounds = true
-            imageView.image = background
-            imageView.center = view.center
-            view.addSubview(imageView)
-            self.view.sendSubviewToBack(imageView)
+        let background = UIImage(named: "boxingRing")
         
-//        let backgroundImage = UIImage(named: "boxingRing")
-//        var backgroundImageView : UIImageView!
-//        backgroundImageView.image = backgroundImage
-//        backgroundImageView.contentMode = .scaleAspectFill
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
+        
+        //        let backgroundImage = UIImage(named: "boxingRing")
+        //        var backgroundImageView : UIImageView!
+        //        backgroundImageView.image = backgroundImage
+        //        backgroundImageView.contentMode = .scaleAspectFill
         imageView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-       
+        
         
         
         
     }
     
-
+    
+    @IBAction func jairi(_ sender: Any) {
+        //        jairi.setImage(UIImage(names: "jairito"), for: .normal)
+        //    }else
+        
+        
+        
+        
+        
+    }
     
     
     
     
     
-
+    //    @IBAction func jared(_ sender: Any) {
+//}
+    
+    
+    
+    
+    
 }
-
